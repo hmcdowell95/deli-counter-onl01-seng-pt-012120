203ae2ad1bfd99e281b1572cc9f5567b6b7dffd8
Harley McDowell <hmcdowell95@gmail.com>
2020-01-30 00:27:14 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,10 +3,12 @@ def line(katz_deli)
   if katz_deli.size == 0
     puts "The line is currently empty."
   else 
-    loop do 
-      phrase = "The line is currently: "
+    katz_deli.each do 
+      phrase = "The line is currently:"
       counter = 1 
-      phrase += "#{name}"
+      phrase += " #{counter}. #{name}"
+      counter += 1 
+    end
   end
 end
 
